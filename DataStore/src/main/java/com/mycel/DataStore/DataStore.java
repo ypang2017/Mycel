c1308b4b6e26691fcc9c539c7225b1f7d187c24c
ypang2017 <798287500@qq.com>
2018-01-26 17:15:25 +0800

    update the TableUtils.java with some new methods,add DataStore.java

---
+++ DataStore/src/main/java/com/mycel/DataStore/DataStore.java
@@ -0,0 +1,4 @@
+package com.mycel.DataStore;
+
+public class DataStore {
+}
---
+++ src/main/java/com/mycel/Configuration.java
@@ -0,0 +1,7 @@
+package com.mycel.conf;
+
+import org.apache.log4j.Logger;
+
+public class Configuration {
+  static final Logger LOG = Logger.getLogger(Configuration.class);
+}
